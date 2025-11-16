ea9c6048d269d3e657d2339d598b874c92097e24
@hassium <108536018+HASSIUM303@users.noreply.github.com>
2025-11-16 15:04:52 +0800

    Update main.py
    why

--- python/main.py
+++ python/main.py
@@ -1,68 +1,19 @@
-import random
+def min_value(nums) -> int:
+    M = nums[0]
 
-text = input("Введи пример (например, 5+3): ")
-
-if "+" in text:
-    sign = "+"
-elif "-" in text:
-    sign = "-"
-elif "*" in text:
-    sign = "*"
-elif "/" in text:
-    sign = "/"
-else:
-    print("Я не понял знак! Используй +, -, * или /")
-    exit()
-
-if sign == "+":
-    parts = text.split("+")
-elif sign == "-":
-    parts = text.split("-")
-elif sign == "*":
-    parts = text.split("*")
-elif sign == "/":
-    parts = text.split("/")
+    for i in range(len(nums)):
+        if nums[i] < M: M = nums[i]
 
 
-first = float(parts[0])
-second = float(parts[1])
+    return M
 
 
-if sign == "+":
-    answer = first + second
-elif sign == "-":
-    answer = first - second
-elif sign == "*":
-    answer = first * second
-elif sign == "/":
-    if second == 0:
-        print("На ноль делить нельзя!")
-        exit()
-    else:
-        answer = first / second
+def sort_string(nums_str = ""):
+    nums = nums_str.split(" ")
+    nums.sort()
+    result = ""
 
-print("Ответ:", answer)
+    for i in nums:
+        result += i + " "
 
-#Second
-
-numbers = []
-for i in range(10):
-    numbers.append(random.randint(-50, 50))
-
-min_num = numbers[0]
-max_num = numbers[0]
-negative_count = 0
-positive_count = 0
-zero_count = 0
-
-for num in numbers:
-   if num < min_num:
-        min_num = num
-   if num > max_num:
-        max_num = num
-
-   if num < 0:
-        negative_count += 1
-   elif num > 0:
-        positive_count
-   else: zero_count += 1 
+    return result
